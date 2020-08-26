8ce1c23282e6bdf63f005d2f8d15e5d3f14a08ea
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-08-25 23:00:08 -0300

    hola2

---
+++ 25082020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             int numero = 0;
+             while (numero < 10)
+             {
+                 numero++;
+                 Console.WriteLine(numero);
+
+             }
+             do
+             {
+                 Console.WriteLine(numero);
+
+             } while (numero < 10);
+             Console.WriteLine("Hello World!");
+         */
+            int intentos = 0;
+            int numero = 2;
+            while (numero != 2 && intentos < 4);
+            {
+                Console.WriteLine("ingrese numero del 1 al 3");
+                numero =int.Parse(Console.ReadLine());
+                Console.WriteLine("bien");
+                intentos++;
+               
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
