16ae9e203c65e2882534f12628d1acf8337ebe3c
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-09-01 22:18:45 -0300

    cals 3

--- 25082020/ConsoleApp1/ConsoleApp1/Program.cs
+++ 25082020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //------------------------------------------------------------------------------
+            // clase 2
+            //------------------------------------------------------------------------------
             /*
              int numero = 0;
              while (numero < 10)
@@ -21,6 +24,7 @@ namespace ConsoleApp1
              } while (numero < 10);
              Console.WriteLine("Hello World!");
          */
+            /*
             int intentos = 0;
             int numero = 2;
             while (numero != 2 && intentos < 4);
@@ -32,6 +36,135 @@ namespace ConsoleApp1
                
             }
             Console.WriteLine("Hello World!");
+            */
+            //------------------------------------------------------------------------------
+            // clase 3
+            //------------------------------------------------------------------------------
+            /*int numeroSecreto;
+            int respuesta;
+            int maxIntentos = 3;
+
+            Random random = new Random();
+            numeroSecreto = random.Next(10);
+
+            string mensaje = "";
+            Console.WriteLine("Intenta adivinar el numero secreto \n");
+
+            for (int i = 0; i < maxIntentos; i++)
+            {
+                respuesta = int.Parse(Console.ReadLine());
+                mensaje = respuesta == numeroSecreto ? "Has Ganado" : "El numero no es correcto. Vuelve a intentar";
+                if (respuesta == numeroSecreto)
+                {
+                    break;
+                }
+
+                if (i == maxIntentos - 1)
+                {
+                    mensaje = "Has perdido el numero era: " + numeroSecreto;
+                    break;
+                }
+                Console.WriteLine(mensaje + "\n");
+            }
+            Console.WriteLine(mensaje + "\n");
+            */
+            //-----------------------------------------------------------------------------------
+            /*
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ingrese el numero");
+                string valor = Console.ReadLine();
+                numero = int.Parse(valor);
+                if (numero == secreto) break;
+            }
+            if (numero == secreto)
+            {
+                Console.WriteLine("Ganaste");
+            }
+            else
+            {
+                Console.WriteLine("Perdiste el numero era " + secreto);
+            }
+            */
+
+            //---------------------------------------------------------------
+            /*
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ingrese el numero");
+                string valor = Console.ReadLine();
+                numero = int.Parse(valor);
+                if (numero == secreto) break;
+            }
+            String mensaje;
+            if (numero == secreto)
+            {
+                mensaje = Ganaste();
+            }
+            else
+            {
+                mensaje = Perdiste(secreto);
+            }
+            Console.WriteLine(mensaje);
+            static string Ganaste()
+            {
+                string mensaje = "ganste";
+                return mensaje;
+            }
+
+            static string Perdiste(int valor)
+            {
+                return "Perdiste" + valor;
+            }
+            */
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                IngreseElNumero("ingrese el numero");
+                if (numero == secreto) break;
+            }
+            String mensaje;
+
+            if (numero == secreto)
+            {
+                mensaje = Ganaste();
+            }
+            else
+            {
+                mensaje = Perdiste(secreto);
+            }
+            Console.WriteLine(mensaje);
+
+            static void IngreseElNumero(string mensaje,ref int numero)
+            {
+                Console.WriteLine(mensaje);
+                string valor = Console.ReadLine();
+                numero = int.Parse(valor);
+            }
+            
+            static string Ganaste()
+            {
+                string mensaje = "ganste";
+                return mensaje;
+            }
+
+            static string Perdiste(int valor)
+            {
+                return "Perdiste" + valor;
+            }
+
+
         }
+        
     }
 }
