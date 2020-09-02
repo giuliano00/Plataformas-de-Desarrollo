2d2dbef23e552e30e54ad119692bb81a6edd22e0
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-09-02 15:30:24 -0300

    clase3

--- 01092020/Consoleapp2/ConsoleApp2/Program.cs
+++ 01092020/Consoleapp2/ConsoleApp2/Program.cs
@@ -1,42 +1,12 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //------------------------------------------------------------------------------
-            // clase 2
-            //------------------------------------------------------------------------------
-            /*
-             int numero = 0;
-             while (numero < 10)
-             {
-                 numero++;
-                 Console.WriteLine(numero);
-
-             }
-             do
-             {
-                 Console.WriteLine(numero);
-
-             } while (numero < 10);
-             Console.WriteLine("Hello World!");
-         */
-            /*
-            int intentos = 0;
-            int numero = 2;
-            while (numero != 2 && intentos < 4);
-            {
-                Console.WriteLine("ingrese numero del 1 al 3");
-                numero =int.Parse(Console.ReadLine());
-                Console.WriteLine("bien");
-                intentos++;
-               
-            }
             Console.WriteLine("Hello World!");
-            */
             //------------------------------------------------------------------------------
             // clase 3
             //------------------------------------------------------------------------------
@@ -184,10 +154,10 @@ namespace ConsoleApp1
                 {
                     return false;
                 }
-
+            }
                 Console.WriteLine(calcularAnioBisiesto(pedirAño("Ingrese su año de nacimiento")));
 
-            }
+            
         }
     }
 }
